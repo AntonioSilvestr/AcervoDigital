c9baefd4e8f5d38a28eab73d024f2e537b572e2c
Silvestrebat <111001890+Silvestrebat@users.noreply.github.com>
2023-06-30 00:43:14 -0300

    Update login.js

--- public/script/login.js
+++ public/script/login.js
@@ -12,5 +12,29 @@ const botaoEnviar = document.getElementById("botao")
 
 botaoEnviar.addEventListener("click", function(e){
     e.preventDefault()
-    transformarEmJson()
+    enviarObjeto(transformarEmJson())
 })
+
+
+function enviarObjeto(dados) {
+    fetch('/entrar', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dados)
+      
+    })
+      .then(response => {
+        console.log(JSON.stringify(dados))
+        if (response.ok) {
+          window.location.href = "/home"
+        } else {
+          console.error('Falha ao enviar o objeto.');
+        }
+      })
+      .catch(error => {
+    
+        console.error('Erro ao enviar o objeto:', error);
+      });
+  }
